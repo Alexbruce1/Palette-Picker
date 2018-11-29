37825482c1bc62e2d1127b6e9ed4bf1a923cc320
Alex Bruce <brucealmightyalex@yahoo.com>
2018-11-29 13:00:26 -0700

    Add functionality to send palettes to the server

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,27 +1,120 @@
 let generateNewPalette = document.querySelector('.generate-palette-button');
 let newPaletteForm = document.querySelector('.save-form');
 let newPaletteName = document.querySelector('.palette-name');
+let colorContainer = document.querySelector('.color-container');
 let color1 = document.querySelector('.color-text-1');
 let color2 = document.querySelector('.color-text-2');
 let color3 = document.querySelector('.color-text-3');
 let color4 = document.querySelector('.color-text-4');
 let color5 = document.querySelector('.color-text-5');
 
+
+getRandomColor = () => {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+let randomColors = [
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor()
+];
+
+
+window.addEventListener('load', () => {
+  createColorBlocks();
+  getSavedPalettes();
+});
+
+let createColorBlocks = () => {
+  colorContainer.innerHTML = ''
+
+  return randomColors.forEach(color => {
+    colorContainer.innerHTML += `
+      <div class="color color${color}" style="background-color:${color}">
+        <h1></h1>
+        <img src="./images/lock.svg" class="lock-icon" alt="">
+        <h2 class="color-text color-text-1">${color.toUpperCase()}</h2>
+      </div>
+    `
+  });
+};
+
+let getSavedPalettes = () => {
+  return fetch('/api/v1/palettes')
+    .then(response => response.json())
+    .then(data => displaySavedPalettes(data))
+    .catch(error => console.log(error.message))
+};
+
+let displaySavedPalettes = (palettes) => {
+  let projectPaletteContainer = document.querySelector('.project-palette-container')
+  palettes.forEach(palette => {
+    projectPaletteContainer.innerHTML += `
+      <article class="saved-palette">
+        <div class="saved-palette-top-row">
+          <h2 class="saved-palette-title">${palette.name}</h2>
+          <img src="./images/garbage.svg" alt="" class="trash-palette">
+        </div>
+        <article class="saved-palette-inner-container">
+          <div class="saved-palette-color saved-palette-color1" style="background-color:${palette.color1}">
+          </div>
+          <div class="saved-palette-color saved-palette-color2" style="background-color:${palette.color2}">
+          </div>
+          <div class="saved-palette-color saved-palette-color3" style="background-color:${palette.color3}">
+          </div>
+          <div class="saved-palette-color saved-palette-color4" style="background-color:${palette.color4}">
+          </div>
+          <div class="saved-palette-color saved-palette-color5" style="background-color:${palette.color5}">
+          </div>
+        </article>
+      </article>
+    `
+  })
+}
+
 generateNewPalette.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log('change Color Palette');
+  randomColors = [
+    getRandomColor(),
+    getRandomColor(),
+    getRandomColor(),
+    getRandomColor(),
+    getRandomColor()
+  ];
+  createColorBlocks();
 });
 
 newPaletteForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  if (!newPaletteName.value.length) {
+    alert('Please name your new palette');
+    return;
+  }
+
   let newPalette = {
-    name: newPaletteName.value,
-    id: Date.now(),
-    'color1': color1.textContent,
-    'color2': color2.textContent,
-    'color3': color3.textContent,
-    'color4': color4.textContent,
-    'color5': color5.textContent,
+    'name': newPaletteName.value,
+    'color1': randomColors[0],
+    'color2': randomColors[1],
+    'color3': randomColors[2],
+    'color4': randomColors[3],
+    'color5': randomColors[4],
   };
-  console.log(newPalette);
-})
+  
+  console.log(JSON.stringify(newPalette));
+
+  let stringifiedPalette = JSON.stringify(newPalette);
+  
+  fetch('/api/v1/palettes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: stringifiedPalette
+  })
+  .then(response => response.json());
+  
+  newPaletteName.value = '';
+});
--- server.js
+++ server.js
@@ -1,8 +1,54 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
 
-app.use(express.static('public'));
+let projects = [
+];
+
+let palettes = [
+  {
+    name: "asdf",
+    color1: "#2fcca",
+    color2: "#a1c2c1",
+    color3: "#5d39a3",
+    color4: "#964f0b",
+    color5: "#76072b"
+  },
+  {
+    name: "asdfgh",
+    color2: "#7f7453",
+    color1: "#200340",
+    color3: "#3b994c",
+    color4: "#4ad984",
+    color5: "#3083f2",
+  },
+];
+
+app.use(express.static('public'), bodyParser.json());
+
+app.get('/api/v1/projects', (request, response) => {
+  return response.status(200).json(projects);
+});
+
+app.post('/api/v1/projects', (request, response) => {
+  // return response.status(200).json(projects);
+});
+
+app.get('/api/v1/palettes', (request, response) => {
+  return response.status(200).json(palettes);
+});
+
+app.post('/api/v1/palettes', (request, response) => {
+  console.log(request.body)
+  // return response.status(200).json('request was successful');
+});
+// 'api/v1/projects/:id/palettes'
+// GET (return all palettes from the back end to the front end)
+// POST (add a new palette to a project)
+// DELETE (delete palette from project)
 
 app.listen(3000, () => {
   console.log('Express intro running on localhost:3000');
 });
+
